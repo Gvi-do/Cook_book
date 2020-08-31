2619ea8a0fec118c0f87dcb3c42d212509369739
Evgeny <zetkornev@gmail.com>
2020-08-31 22:07:53 +0300

    Составляет словарь из рецептов

--- cook_book.py
+++ cook_book.py
@@ -1,22 +1,26 @@
 def book_processing():
   cook_book = {}
-  all_ingredients = []
+  
   
   with open('recipes.txt') as f:
-    name_of_dish = f.readline()
-    number_of_ingredients = int(f.readline())
-    i = 0
-    while i < number_of_ingredients:
-      ingredient = {}
-      string = f.readline()
-      ingredient['ingredient_name'] = string.split('|')[0]
-      ingredient['quantity'] = string.split('|')[1]
-      ingredient['measure'] = string.split('|')[2]
-      
-      i += 1
-      all_ingredients.append(ingredient)
+    for line in f:
+      all_ingredients = []
+      name_of_dish = line
+      number_of_ingredients = int(f.readline())
+      i = 0
+      while i < number_of_ingredients:
         
-  cook_book[name_of_dish] = all_ingredients
+        ingredient = {}
+        string = f.readline()
+        ingredient['ingredient_name'] = string.split('|')[0]
+        ingredient['quantity'] = string.split('|')[1]
+        ingredient['measure'] = string.split('|')[2]
+      
+        i += 1
+        all_ingredients.append(ingredient)
+      f.readline() 
+      cook_book[name_of_dish] = all_ingredients
+        
   return cook_book
 print(book_processing())
 
